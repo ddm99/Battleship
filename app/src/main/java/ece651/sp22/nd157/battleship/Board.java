c853b9fe8d5e71c1ab0f5f27469b46df50a737e0
Ding Ning <zcapndi@gmail.com>
2022-01-19 13:44:16 -0500

    1st task completed

---
+++ app/src/main/java/ece651/sp22/nd157/battleship/Board.java
@@ -0,0 +1,7 @@
+package ece651.sp22.nd157.battleship;
+
+public interface Board {
+  public int getWidth();
+
+  public int getHeight();
+}
---
+++ app/src/test/java/ece651/sp22/nd157/battleship/BoardTest.java
@@ -0,0 +1,13 @@
+package ece651.sp22.nd157.battleship;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+  @Test
+  public void test_() {
+
+  }
+
+}

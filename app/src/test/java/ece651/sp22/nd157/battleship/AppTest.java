6f40d608800c1e255f53aca21a4a00f437c79248
Ding Ning <zcapndi@gmail.com>
2022-01-19 23:57:31 -0500

    task 9 finished

--- app/src/test/java/ece651/sp22/nd157/battleship/AppTest.java
+++ app/src/test/java/ece651/sp22/nd157/battleship/AppTest.java
@@ -4,9 +4,11 @@
 package ece651.sp22.nd157.battleship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.io.StringReader;
 
@@ -45,5 +47,30 @@ class AppTest {
     assertEquals(prompt + "\n  0|1\nA  |s A\nB  |  B\n  0|1\n", bytes.toString());
     bytes.reset(); // clear out bytes for next time around
   }
+  
+  @Test
+    public void test_main() throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(bytes, true);
+        InputStream input = getClass().getClassLoader().getResourceAsStream("input.txt");
+        assertNotNull(input);
+        InputStream expectedStream = getClass().getClassLoader().getResourceAsStream("output.txt");
+        assertNotNull(expectedStream);
+        InputStream oldIn = System.in;
+        PrintStream oldOut = System.out;
+        try {
+            System.setIn(input);
+            System.setOut(out);
+            App.main(new String[0]);
+        }
+        finally {
+            System.setIn(oldIn);
+            System.setOut(oldOut);
+        }
+        String expected = new String(expectedStream.readAllBytes());
+        String actual = bytes.toString();
+        assertEquals(expected, actual);
+    }
+
 
 }
